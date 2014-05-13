39ece89039b52ca889d87e3d4265eff6bdb1bfe3
tatyanos <tatyana.khoreva@gmail.com>
2014-05-14 01:17:50 +0400

    first commit

---
+++ asks/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for asks project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asks.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
