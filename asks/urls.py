401cc8e926649c7e2932e051f32427e2868c31b4
tatyanos <tatyana.khoreva@gmail.com>
2014-06-01 17:44:05 +0400

    add first view

--- asks/urls.py
+++ asks/urls.py
@@ -3,10 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'asks.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
+    url(r'^app/', include('app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
