d80d9172dbdd0093bcea017bb05a06dc08727105
tatyanos <tatyana.khoreva@gmail.com>
2014-06-01 18:02:10 +0400

    add easy views

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from app import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>\d+)/like/$', views.like, name='like'),
 )
