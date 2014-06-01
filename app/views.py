dc89aff746467b1fb88e8fffec161cc93f87309b
tatyanos <tatyana.khoreva@gmail.com>
2014-06-01 19:40:01 +0400

    add render instead HttpResponse

--- app/views.py
+++ app/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 from app.models import Question
 
 
 def index(request):
-    #return HttpResponse("Hello, world. You're at the question index.")
     latest_question_list = Question.objects.order_by('-date')[:5]
-    template = loader.get_template('app/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'app/index.html', context)
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
