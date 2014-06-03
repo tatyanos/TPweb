868dfe11e4e1c843c4079bb2c96b08fc15fd4163
tatyanos <tatyana.khoreva@gmail.com>
2014-06-03 04:44:45 +0400

    add mysql, generate db bulk_create

--- generate_db.py
+++ generate_db.py
@@ -1,50 +1,55 @@
 import os
 import random
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asks.settings")
 
 from app.models import User, Question, Answer, AnswerLike, QuestionLike
 
-User.objects.exclude(username='tatyana').delete()
-Question.objects.all().delete()
-Answer.objects.all().delete()
-AnswerLike.objects.all().delete()
-QuestionLike.objects.all().delete()
+USERS = 10000
+QUESTIONS = 100000
 
-
-def get_like_value():
-    return 2 * random.randint(0, 1) - 1
-
-names = ['q', 'w', 'e', 'r', 't', 'y']
 users = []
-for i in xrange(10):
-    user = User.objects.create_user('user' + str(i + 1), 'mail' + str(i + 1) + '@mail.ru', '1')
-    users.append(user)
+for i in xrange(USERS):
+    users.append(User(username='user' + str(i + 1), email='mail' + str(i + 1) + '@mail.ru', password='1'))
+    if i % 1000 == 0:
+        print 'u' + str(i)
+User.objects.bulk_create(users)
+users = None
 
 questions = []
-for i in xrange(101):
-    obj = Question(title='Question' + str(i) + random.choice(names) + '?',
-                   text='text' + random.choice(names),
-                   user=random.choice(users))
-    obj.save()
-    questions.append(obj)
+for i in xrange(QUESTIONS):
+    questions.append(Question(title='Question' + str(i) + '?',
+                              text='text' + str(i),
+                              user_id=random.randint(1, USERS)))
+    if i % 1000 == 0:
+        print 'q' + str(i)
+Question.objects.bulk_create(questions)
+questions = None
 
-answers = []
-for i in xrange(200):
-    obj = Answer(text='Answer' + str(i) + random.choice(names),
-                 user=random.choice(users),
-                 question=random.choice(questions))
-    obj.save()
-    answers.append(obj)
+for j in xrange(20):
+    answers = []
+    for i in xrange(QUESTIONS / 2):
+        answers.append(Answer(text='Answer' + str(i),
+                              user_id=random.randint(1, USERS),
+                              question_id=random.randint(1, QUESTIONS)))
+        if i % 1000 == 0:
+            print 'a' + str(j) + '_' + str(i)
+    Answer.objects.bulk_create(answers)
+
+answers = None
 
 
-for i in xrange(5):
-    obj = AnswerLike(value=get_like_value(),
-                     user=random.choice(users),
-                     answer=random.choice(answers))
-    obj.save()
-
-for i in xrange(5):
-    obj = QuestionLike(value=get_like_value(),
-                       user=random.choice(users),
-                       question=random.choice(questions))
-    obj.save()
+# def get_like_value():
+#     return 2 * random.randint(0, 1) - 1
+#
+# for i in xrange(5):
+# obj = AnswerLike(value=get_like_value(),
+#                      user=random.choice(users),
+#                      answer=random.choice(answers))
+#     obj.save()
+#
+# for i in xrange(5):
+#     obj = QuestionLike(value=get_like_value(),
+#                        user=random.choice(users),
+#                        question=random.choice(questions))
+#     obj.save()
